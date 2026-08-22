86bcaf4145003b7de8b5fcb1f72c1f2de190a169
Oleksandr Romaniuk <romanuksashko@gmail.com>
2026-08-22 09:08:44 +0300

    PR1

---
+++ practice03/greeting.py
@@ -0,0 +1,16 @@
+# Personalized greeting program
+name = "Oleksandr"
+surname = "Romaniuk"
+group = "IT-31"
+birth_year = 2008
+current_year = 2026
+
+age = current_year - birth_year
+full_name = name + " " + surname
+
+print("Hello, world!")
+print("My name is", full_name)
+print("Group:", group)
+print("I am", age, "years old")
+print("Letters in my full name:", len(full_name))
+print("Loud version:", full_name.upper())
---
+++ practice03/python-online.py
@@ -0,0 +1,10 @@
+# Online version: data is set directly in the code
+name = "Oleksandr"
+group = "IT-31"
+editor = "VS Code"
+
+print("Hello from an online interpreter!")
+print("Author:", name)
+print("Group:", group)
+print("My local editor:", editor)
+print("2 ** 10 =", 2 ** 10)
